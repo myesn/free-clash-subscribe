c25e7e9660e9c71f3440c172e3e5186849a3d560
myesn <myesn@foxmail.com>
2024-01-18 18:50:33 +0800

    freeclash reverse

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,21 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import dayjs from 'dayjs'
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const clashSubscribeAddress = getClashSubscribeAddress()
+  const response = await fetch(clashSubscribeAddress)
+  const configYaml = await response.text()
+
+  res.setHeader('Content-Type', 'application/octet-stream')
+
+  return res.send(configYaml)
+}
+
+function getClashSubscribeAddress() {
+  const now = dayjs()
+  const year = now.format('YYYY')
+  const month = now.format('MM')
+  const day = now.format('DD')
+
+  return `https://freeclash.org/wp-content/uploads/${year}/${month}/${month}${day}.yaml`
 }
